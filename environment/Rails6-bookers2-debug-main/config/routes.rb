3d8170fe124e3822ba80f93f7aac26f71855ab5c
kazuki-05 <ec2-user@ip-172-31-9-243.ap-northeast-1.compute.internal>
2022-08-09 06:41:58 +0000

    search

--- environment/Rails6-bookers2-debug-main/config/routes.rb
+++ environment/Rails6-bookers2-debug-main/config/routes.rb
@@ -12,15 +12,15 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show,:index,:edit,:update]
   get "home/about"=>"homes#about"
-  
+
     # ネストさせる
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
 
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
