a26a91909fb18739b34a9d76c62874a977853540
kazuki-05 <ec2-user@ip-172-31-9-243.ap-northeast-1.compute.internal>
2022-10-18 16:20:49 +0000

    js

---
+++ environment/JS2/app.js
@@ -0,0 +1,52 @@
+// let hello = 'Hello World';
+// alert(hello);
+
+// 四則演算
+// alert(4+3);
+
+// alert(8-5);
+
+// alert(2*6);
+
+// alert(10/2);
+
+// alert('Hello'+'World');
+
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1+str2);
+
+
+
+// 条件分岐
+// let orange = 100
+// let apple = 120
+
+// if(orange<apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num*2;
+//   count = count+1;
+// }
+
+// alert('2でかけ続けて'+max+'を越えるのに必要な回数は'+count+'回です')　
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num+i;
+}
+
+alert('1から10まで足し算した結果は'+ num +'です');
+
